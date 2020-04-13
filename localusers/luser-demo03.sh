018df10a2ef06df93a5d24a3289b07440d5e1ae4
Daniel Olaya <d_olaya@hotmail.com>
2020-04-12 18:12:44 -0700

    Examples with test built-in

--- localusers/luser-demo03.sh
+++ localusers/luser-demo03.sh
@@ -10,7 +10,7 @@ echo "Your UID is ${UID}"
 UID_TO_TEST_FOR='1000'
 if [[ "${UID}" -ne "${UID_TO_TEST_FOR}" ]]
 then
-	echo "hi ${UID_TO_TEST_FOR}"
+	echo "Your UID does not match ${UID_TO_TEST_FOR}"
 	exit 1
 fi
 
@@ -18,5 +18,24 @@ fi
 USER_NAME=$(id -un)
 
 # Test if the command succeeded.
+if [[ "${?}" -ne 0 ]]
+then
+	echo 'The id command did not execute succesfully.'
+	exit 1
+fi
+echo "Your username is ${USER_NAME}"
 
 # test for != for the string.
+USER_NAME_TO_TEST_FOR='casa'
+if [[ "${USER_NAME}" = "${USER_NAME_TO_TEST_FOR}" ]]
+then
+	echo "your username matches ${USER_NAME_TO_TEST_FOR}."
+fi
+
+if [[ "${USER_NAME}" != "${USER_NAME_TO_TEST_FOR}" ]]
+then
+	echo "Your username does not match ${USER_NAME_TO_TEST_FOR}."
+	exit 1
+fi
+
+exit 0
