023370c0dd83dc2a62b1ba97df340908c52f7df9
Daniel Olaya <d_olaya@hotmail.com>
2020-04-13 19:07:39 -0700

    Paths intro

---
+++ localusers/luser-demo06.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This script generates a random password for each user specirfied on the command line
+
+# Display what the user typed on the command line.
+echo "You executed this command: ${0}"
+
+
