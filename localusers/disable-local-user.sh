f32dbec216a19eb7e86852ba9666f2bfa91076cf
Daniel Olaya <d_olaya@hotmail.com>
2020-04-19 15:18:35 -0700

    Disable, remove/archive home, delete user

--- localusers/disable-local-user.sh
+++ localusers/disable-local-user.sh
@@ -4,6 +4,7 @@
 
 usage () {
 	echo "Usage: ${0} USERNAME" >&2
+	exit 1
 }
 
 if [[ "${UID}" -ne 0 ]]
@@ -33,35 +34,44 @@ do
 	esac
 done
 
+
 # Remove the options while leaving the reamaining arguments
 shift "$(( OPTIND - 1 ))"
 if [[ "${#}" -eq 0 ]]
 then 
 	usage
-	exit 1
 fi
+
 
 # Processing user list
 while [[ "${#}" -gt 0 ]]
 do
   echo
   USER=${1}
-  echo "Processing request to delete user: ${USER}"
   ID=$(id -u ${USER} 2>/dev/null)
   if [[ "${?}" -ne 0 ]]
   then
   	echo "Could NOT find user ${USER} in the system."
+  	shift
+	continue
   fi	
   
   if [[ ! -z "${ID}" ]] && [[ ${ID} -lt 1000 ]]
   then
   	echo "Cannot delete system account, skipping user ${USER} deletion with id: ${ID}"
+  	shift
+	continue
   fi 
 
-  if [[ ARCHIVE ]]
+
+  if [[ "${ARCHIVE}" = true ]]
   then
-	echo "Archiving home..."
-	tar -zcvf ${USER}.tar.gz /home/${USER}/
+	if [[ ! -d /archives ]]
+	then
+		mkdir /archives
+	fi
+	echo "Archiving home for user: ${USER}..."
+	tar -zcvf /archives/${USER}.tar.gz /home/${USER}/ &>/dev/null
 	if [[ "${?}" -ne 0 ]]
 	then
 		echo "Error occurred creating archive, skipping user: ${USER} deletion"
@@ -69,7 +79,48 @@ do
 		continue
 	fi
   fi
-  #userdel ${USER}
+  
+  
+  if [[ "${DELETE}" = true ]]
+  then
+	echo "Deleting user: ${USER}, deleting home dir"
+	userdel --remove ${USER}
+	shift
+	continue
+  fi
+  if [[ "${?}" -ne 0 ]]
+  then
+	echo "ERROR deleting user: ${USER}"
+	shift
+	continue
+  
+  fi
 
+
+  if [[ "$REMOVE_HOME" = true ]]
+  then
+	echo "Removing home dir, user: ${USER}"
+	rm -r /home/${USER}
+  fi
+  if [[ "${?}" -ne 0 ]]
+  then
+	echo "ERROR removing home dir, user: ${USER}"
+	shift
+	continue
+  
+  fi
+
+
+  chage -E0 ${USER}
+  if [[ "${?}" -ne 0 ]]
+  then
+  	echo "Error disabling user: ${USER} account"
+	shift
+	continue
+  fi
+
+  echo "User: ${USER} suspended succesfully."
   shift 
 done
+
+exit 0
