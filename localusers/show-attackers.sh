c57011c334c0d4a7ae3a240748473f4ece92f61c
Daniel Olaya <d_olaya@hotmail.com>
2020-04-20 18:07:09 -0700

    Refactored syslog analyzer script

--- localusers/show-attackers.sh
+++ localusers/show-attackers.sh
@@ -1,38 +1,28 @@
 #!/bin/bash
 
-# Analyzes syslog file to find number of failed login attempts per ip.
 
+# Analyzes syslog file to find number of failed login attempts per ip.
+# If there are any IPs with over LIMIT failures, display the count, IP and location.
+
+LIMIT='10'
 FILE=${1}
+
 if [[ ! -e "${FILE}" ]]
 then
-  echo "File does not exit, terminating program."
+  echo "File does not exit, terminating program, ${FILE}" >&2
   exit 1
 fi
 
-
-TMP="./tmp-totals.txt"
-TMP2="./tmp-totals2.txt"
-TOTALS="./totals.csv"
-cat ./${FILE} | grep -E "\bFailed password for invalid user" | grep -v "snapshot" | awk '{print $13}' > ${TMP}
-cat ./${FILE} | grep -E "\bFailed password for root" | awk '{print $11}' >> ${TMP}
-cp ${TMP} ${TMP2}
-cat ${TMP} | sort -n | uniq -c | sort -nr > ${TMP2}
-
-
-echo "Count,IP,Location" > ${TOTALS}
-cat ${TMP2} | while read LINE
+echo "Count,IP,Location"
+grep Failed ${FILE} | awk -F 'from ' '{print $2}' | awk '{print $1}' | sort | uniq -c | sort -nr | while read COUNT IP
 do
-	NUM_ATTEMPTS=$(echo "${LINE}" | awk '{print $1}')
-	if [[ "${NUM_ATTEMPTS}" -gt 9 ]]
+	if [[ "${COUNT}" -gt "${LIMIT}" ]]
 	then
-		IP=$(echo "${LINE}" | awk '{print $2}')
-		LOCATION=$(geoiplookup ${IP} | awk -F ":" '{print $2}')
-		echo "${NUM_ATTEMPTS},${IP},${LOCATION}" >> ${TOTALS}
+		LOCATION=$(geoiplookup ${IP} | awk -F ', ' '{print $2}')
+		echo "${COUNT},${IP},${LOCATION}"
 	fi
 done
 
-
-rm ${TMP} ${TMP2}
 exit 0
 
 
