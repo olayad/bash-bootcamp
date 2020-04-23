3cbe7f53501464c359bb4e1659b56097fbb190da
Daniel Olaya <d_olaya@hotmail.com>
2020-04-23 16:15:17 -0700

    Refactored script

--- multinet/run-everywhere.sh
+++ multinet/run-everywhere.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-#
+# Executes an ssh in multiple hosts
 
 SERVERS_FILE='/vagrant/servers'
 
+# Options for the ssh command
+SSH_OPTIONS='-o ConnectTimeout=2'
 
 usage() {
-  echo 'This script allows a user to execute commands in multiple hosts.'
-  echo 'This script should not be run as superuser.'
-  echo 'Usage: ${0} [-vsn] [-f FILE]' 
-  echo '  -v	Verbose mode.'
-  echo '  -n	Dry run commands, display instead of execute.' 
-  echo '  -f 	Provide a file that includes hosts names.'
-  echo '  -s 	Run commands in host as sudo.'
+  echo "Usage: ${0} [-vsn] [-f FILE] COMMAND"
+  echo 'This script allows a user to execute commands in multiple hosts.' 
+  echo 'This script should not be run as superuser.' 
+  echo '  -v	Verbose mode.' 
+  echo '  -n	Dry run mode, display COMMAND instead of execute.' 
+  echo "  -f 	Provide a file that includes hosts names. Default ${SERVER_FILE}" 
+  echo '  -s 	Execute COMMAND in host as sudo.' 
+  exit 1
 }
 
 
@@ -33,10 +36,9 @@ log() {
 # Enforce that script is NOT being run as sudo
 if [[ "${UID}" -eq 0 ]]
 then
-  log 'Script should not be run as superuser.' 2
+  log 'Script should not be run as superuser, user -s option instead.' 2
   usage
 fi 
-
 
 
 # Process input parameters
@@ -45,7 +47,7 @@ do
   case ${OPTION} in
     f)  SERVERS_FILE="${OPTARG}" ;;
     n)  DRY_RUN='true' ;;
-    s)  SUPERUSER='true' ;;
+    s)  SUDO='sudo' ;;
     v)  VERBOSE='true' ;;
     ?)  usage  ;;
   esac
@@ -57,32 +59,36 @@ if [[ "${#}" -eq 0 ]]
 then 
   usage
 fi
+# Anything that remains, treat it as a single command
+COMMAND=${@}
 
 
 # Check if servers file exists
 log 'Checking if server file exists...' 1
 if  [[ ! -e "${SERVERS_FILE}" ]]
 then
-  log "Servers file could not be opened: ${SERVERS_FILE}" 2
+  echo "Servers file could not be opened: ${SERVERS_FILE}" >&2
   exit 1
 fi
 
-
+EXIT_STATUS='0'
 for SERVER in $(cat ${SERVERS_FILE})
 do
-  MESSAGE="Executing command:"${@}" @:"${SERVER}
-  log "${MESSAGE}" 1
-  if [[ "${SUPERUSER}" = 'true' ]]
+  SSH_COMMAND="ssh ${SSH_OPTIONS} ${SERVER} ${SUDO} ${COMMAND}"
+  log "Executing command:${SSH_COMMAND} @:${SERVER}" 1
+  if  [[ "${DRY_RUN}" = 'true' ]]
   then
-    sudo ssh ${SERVER} ${@} 2>/dev/null
+    echo "DRY RUN: ${SSH_COMMAND}"
   else
-    ssh ${SERVER} ${@} 2>/dev/null
+    ${SSH_COMMAND}
+    SSH_EXIT_STATUS="${?}"
   fi
-  if [[ "${?}" -ne 0 ]] 
+  if [[ "${SSH_EXIT_STATUS}" -ne 0 ]] 
   then
-     log "Command was not executed succesfully.	${@}@:${SERVER}" 2
+     EXIT_STATUS="${SSH_EXIT_STATUS}"
+     log "Execution failed. ${SSH_COMMAND}@:${SERVER}" 2
   fi
   echo
 done
 
-exit 0
+exit ${EXIT_STATUS}
